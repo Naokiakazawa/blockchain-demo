7a536142618797b124106b23ebd50c1b4db41fce
Naokiakazawa <naoki9akazawa@gmail.com>
2019-12-22 22:27:48 +0900

    output every second

--- cmds/receive.sh
+++ cmds/receive.sh
@@ -4,6 +4,10 @@ count_blocks () {
     block_count=$(find /home/data/blocks -type f | wc -l)
     str="同期したブロック数→　${block_count}"
     echo $str
+    sleep 1s
 }
 
-count_blocks
+while true
+do
+    count_blocks
+done
