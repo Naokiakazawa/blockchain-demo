af6450abce2dbdb0fbbfeb96672cd4835b7a90e5
Naoki Akazawa <naoki9akazawa@gmail.com>
2019-12-23 14:40:46 +0900

    hide error outputs

--- cmds/receive.sh
+++ cmds/receive.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 count_blocks () {
-    block_count=$(ls -U1 /home/data/blocks | wc -l)
+    block_count=$(ls -U1 /home/data/blocks 2>/dev/null | wc -l)
     str="同期したブロック数→　${block_count}"
     echo $str
     sleep 1s
--- cmds/transfer.sh
+++ cmds/transfer.sh
@@ -8,6 +8,6 @@ sshpass -p "password" ssh -o "StrictHostKeyChecking no" $USER@$RECEIVER mkdir -p
 
 while true
 do
-	sshpass -p "password" rsync -au $DIR/ $USER@$RECEIVER:$DIR/
+	sshpass -p "password" rsync -au $DIR/ $USER@$RECEIVER:$DIR/ 2>/dev/null
     sleep 0.5s
 done
