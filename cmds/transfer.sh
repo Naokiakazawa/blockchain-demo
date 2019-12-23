f12d69522dfe65b22fa335cacd76956af04821c0
Naoki Akazawa <naoki9akazawa@gmail.com>
2019-12-23 17:49:02 +0900

    fix indents

--- cmds/transfer.sh
+++ cmds/transfer.sh
@@ -9,5 +9,5 @@ sshpass -p "password" ssh -o "StrictHostKeyChecking no" $USER@$RECEIVER mkdir -p
 while true
 do
 	sshpass -p "password" rsync -auv $DIR/ $USER@$RECEIVER:$DIR/ 2>/dev/null
-    sleep $2s
+	sleep $2s
 done
