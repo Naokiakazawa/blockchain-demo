06735e8a768eeaa911c84e5f76ebc28a73fa062f
Naoki Akazawa <naoki9akazawa@gmail.com>
2019-12-29 16:39:36 +0900

    modify tranfer proccess in order to transfer each block

--- cmds/receive.sh
+++ cmds/receive.sh
@@ -4,10 +4,10 @@ count_blocks () {
     block_count=$(ls -U1 /home/data/blocks 2>/dev/null | wc -l)
     str="同期したブロック数→　${block_count}"
     echo $str
-    sleep 1s
 }
 
 while true
 do
     count_blocks
+    sleep 1s
 done
--- cmds/transfer.sh
+++ cmds/transfer.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/bash
 
-DIR=/home/data/blocks
+DIR=/home/data/blocks/
 RECEIVER=$1
 USER=root
+BLOCKNUM=0
+
+transfer_block(){
+	N="000${BLOCKNUM}"
+	sshpass -p "password" rsync -au $DIR/block${N: -3}.json $USER@$RECEIVER:$DIR 2>/dev/null
+	echo -e "Transfer #${N: -3} block !\n"
+	$((BLOCKNUM++)) 2>/dev/null
+}
 
 sshpass -p "password" ssh -o "StrictHostKeyChecking no" $USER@$RECEIVER mkdir -p $DIR
 
 while true
 do
-	sshpass -p "password" rsync -auv $DIR/ $USER@$RECEIVER:$DIR/ 2>/dev/null
+	transfer_block
 	sleep $2s
 done
